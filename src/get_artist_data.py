c6dffdb55433cea7b8faa08fb75e11f190d9459a
Eric Xu <eric.x3254@gmail.com>
2025-11-19 20:51:50 -0500

    minor changes

--- src/get_artist_data.py
+++ src/get_artist_data.py
@@ -17,10 +17,10 @@ def get_auth_code() -> str:
     # If we already have a valid token, return it
     if TOKEN and time.time() < TOKEN_EXPIRY:
         return TOKEN
-    url = "https://accounts.spotify.com/api/token"
+    url = redirect_uri
     data = {"grant_type": "client_credentials"}
     # From my own spotify developer app "My App"
-    auth = ("f56b6b57452c45838b7d6936ccfbe16e", "d01aa5dc31534bc5a09e993227e46bfc")
+    auth = (client_id, client_secret)
 
     response = requests.post(url, data=data, auth=auth)
     result = response.json()
